fb880c6753c8d3fad5c1375f480b92b1cabe23e9
EvanderVegt <e.vandervegt03@gmail.com>
2017-11-07 10:37:57 +0100

    stap1

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Zeeslag
+        </title>
+        <script>
+
+            function myFunction() {
+            document.getElementById("myImg").src = "plons.jpg";
+            }
+        </script>
+    </head>
+    <body>
+        
+        <?php
+
+        ?>
+        <img onclick="myFunction()" id="myImg" src="golven.png" width="25" height="25">
