ef51071e6b3b63e0601f6325931d10b2a8784af6
EvanderVegt <e.vandervegt03@gmail.com>
2017-11-07 15:33:54 +0100

    stap4

--- index.php
+++ index.php
@@ -11,24 +11,39 @@ and open the template in the editor.
             Zeeslag
         </title>
         <script>
-
-            function plons(a) {
-            document.getElementById("fontijn"+a).src = "plons.jpg";
-            }
             
-            function vuur() {
-            document.getElementById("fakkel").src = "vuur.png";
+            
+           
+    
+                        
+            function plons(a) {
+                document.getElementById("fontijn" + a).src = "plons.jpg";
+                
+            }
+
+            function vuur(b) {
+                document.getElementById("fakkel" + b).src = "vuur.png";
             }
         </script>
     </head>
     <body>
-        
-        <?php
-        for($x=1; $x <=10; $x++){
-            echo "<img onclick=plons(".$x.") id=fontijn".$x." src=golven.png width=25 height=25>";
 
-        }
+        <table id="table1">
+            <?php
+            for ($x = 0; $x <= 9; $x++) {
+                echo "<tr><img onclick=vuur(" . $x . ") id=fakkel" . $x . " src=golven.png width=35 height=35></tr>\n";
 
-        ?>
-        <img onclick="plons($x)" id="fontijn" src="golven.png" width="25" height="25">
-         <img onclick="vuur()" id="fakkel" src="golven.png" width="25" height="25">
+                for ($y = 0; $y <= 9; $y++) {
+                    echo "<td><img onclick=plons($x$y) id=fontijn" . $x.$y . " src=golven.png width=35 height=35>\n";
+                }
+                echo"</td>";
+            }
+            ?>
+        </table>
+
+
+
+
+    <!---<img onclick="plons($x)" id="fontijn" src="golven.png" width="25" height="25">
+     <img onclick="vuur()" id="fakkel" src="golven.png" width="25" height="25">
+ </<body>
